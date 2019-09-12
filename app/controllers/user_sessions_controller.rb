ce5785e969b041151bc4fcc9c2f2ae288c093fec
takakotmt <st.0614.tmt@gmail.com>
2019-09-12 15:39:43 +0900

    掲示板一覧完了。carrierwave追加

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -4,12 +4,15 @@ class UserSessionsController < ApplicationController
 
   def create
     user = login(params[:user_sessions][:email], params[:user_sessions][:password])
-    puts user
-    puts "ssssss"
     if user
       redirect_to top_index_path
     else
       render :new
     end
+  end
+
+  def destroy
+    logout
+    redirect_to(:users, notice: 'Logged out!')
   end
 end
